f08fa4e666347e68cf8ec079d87d81a5d83ba5f9
xu.6 <48320339+xu61029@users.noreply.github.com>
2024-05-28 23:24:24 +0800

    初步建置

---
+++ Final/FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalApp.swift
+//  Final
+//
+//  Created by haohaoxiao on 2024/5/28.
+//
+
+import SwiftUI
+
+@main
+struct FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
